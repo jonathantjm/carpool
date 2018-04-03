38885aa51dc3a1d3056903789ea9a2fedfee977b
Justin Poh <e0031499@u.nus.edu>
2018-04-03 19:37:43 +0800

    Modified adminCreateOffer to fit bigserial type on advertisementID.

--- htdocs/adminCreateOffer.php
+++ htdocs/adminCreateOffer.php
@@ -14,7 +14,6 @@ $max_advertisementID = pg_query($db, "SELECT max(advertisementID) FROM advertise
 
 if (isset($_POST['submit'])) {
 
-    $advertisementID = pg_fetch_array($max_advertisementID)[0] + 1;
     $email = $_POST['email'];
     $start_location = $_POST['start_location'];
     $end_location = $_POST['end_location'];
@@ -22,7 +21,7 @@ if (isset($_POST['submit'])) {
     $time_of_pickup = $_POST['time_of_pickup'];
     $self_select = $_POST['self_select'];
 
-    pg_query_params("INSERT INTO advertisements (advertisementid, email_of_driver, start_location, end_location, creation_date_and_time, date_of_pickup, time_of_pickup, self_select) VALUES ($1, $2, $3, $4, NOW(), $5, $6, $7)", array($advertisementID, $email, $start_location, $end_location, $date_of_pickup, $time_of_pickup, $self_select));
+    pg_query_params("INSERT INTO advertisements (email_of_driver, start_location, end_location, creation_date_and_time, date_of_pickup, time_of_pickup, self_select) VALUES ($1, $2, $3, NOW(), $4, $5, $6)", array($email, $start_location, $end_location, $date_of_pickup, $time_of_pickup, $self_select));
 
     $row[0] = $email;
     $row[1] = $start_location;
